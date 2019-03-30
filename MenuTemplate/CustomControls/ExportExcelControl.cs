c5eaecf65b6b190b957eb1231ddf5b8039b2e9f2
Jesus Noh <felipe_pti@hotmail.com>
2019-03-30 00:41:33 -0600

    se agrega try cach en boton exportar

--- MenuTemplate/CustomControls/ExportExcelControl.cs
+++ MenuTemplate/CustomControls/ExportExcelControl.cs
@@ -24,112 +24,118 @@ namespace RegistryTime.CustomControls
 
         private void ExportExcelButton_Click(object sender, EventArgs e)
         {
-            if(data != null && data.Rows.Count > 0)
-            {
-                SaveFileDialog fichero = new SaveFileDialog();
-                fichero.Filter = "Excel (*.xls)|*.xls";
-                if (fichero.ShowDialog() == DialogResult.OK)
+            try {
+
+                if (data != null && data.Rows.Count > 0)
                 {
-
-                    //Creating DataTable.
-                    DataTable dt = new DataTable();
-
-                    //Adding the Columns.
-                    foreach (DataGridViewColumn column in data.Columns)
+                    SaveFileDialog fichero = new SaveFileDialog();
+                    fichero.Filter = "Excel (*.xls)|*.xls";
+                    if (fichero.ShowDialog() == DialogResult.OK)
                     {
-                        dt.Columns.Add(column.HeaderText, column.ValueType);
-                    }
 
-                    //Adding the Rows.
-                    foreach (DataGridViewRow row in data.Rows)
-                    {
-                        dt.Rows.Add();
-                        foreach (DataGridViewCell cell in row.Cells)
+                        //Creating DataTable.
+                        DataTable dt = new DataTable();
+
+                        //Adding the Columns.
+                        foreach (DataGridViewColumn column in data.Columns)
                         {
-                            Console.WriteLine(cell.ValueType);
+                            dt.Columns.Add(column.HeaderText, column.ValueType);
+                        }
 
-                            if (string.IsNullOrEmpty(cell.Value.ToString()))
+                        //Adding the Rows.
+                        foreach (DataGridViewRow row in data.Rows)
+                        {
+                            dt.Rows.Add();
+                            foreach (DataGridViewCell cell in row.Cells)
                             {
-                                if (cell.ValueType == typeof(Int32)) cell.Value = 0;
-                                if (cell.ValueType == typeof(Decimal)) cell.Value = 0;
-                                if (cell.ValueType == typeof(Double)) cell.Value = 0;
-                                if (cell.ValueType == typeof(String)) cell.Value = string.Empty;
-                                if (cell.ValueType == typeof(Boolean)) cell.Value = false;
-                                if (cell.ValueType == typeof(DateTime)) cell.Value = string.Empty;
+                                Console.WriteLine(cell.ValueType);
+
+                                if (string.IsNullOrEmpty(cell.Value.ToString()))
+                                {
+                                    if (cell.ValueType == typeof(Int32)) cell.Value = 0;
+                                    if (cell.ValueType == typeof(Decimal)) cell.Value = 0;
+                                    if (cell.ValueType == typeof(Double)) cell.Value = 0;
+                                    if (cell.ValueType == typeof(String)) cell.Value = string.Empty;
+                                    if (cell.ValueType == typeof(Boolean)) cell.Value = false;
+                                    if (cell.ValueType == typeof(DateTime)) cell.Value = string.Empty;
+                                }
+                                dt.Rows[dt.Rows.Count - 1][cell.ColumnIndex] = cell.Value.ToString();
                             }
-                            dt.Rows[dt.Rows.Count - 1][cell.ColumnIndex] = cell.Value.ToString();
                         }
-                    }
 
-                    //Exporting to Excel.
-                    //string folderPath = "C:\\Excel\\";
-                    //if (!Directory.Exists(folderPath))
-                    //{
-                    //    Directory.CreateDirectory(folderPath);
-                    //}
-                    using (XLWorkbook wb = new XLWorkbook())
-                    {
-                        wb.Worksheets.Add(dt, Title);
-
-                        ////Set the color of Header Row.
-                        ////A resembles First Column while C resembles Third column.
-                        //wb.Worksheet(1).Cells("A1:C1").Style.Fill.BackgroundColor = XLColor.DarkGreen;
-                        //for (int i = 1; i <= dt.Rows.Count; i++)
+                        //Exporting to Excel.
+                        //string folderPath = "C:\\Excel\\";
+                        //if (!Directory.Exists(folderPath))
                         //{
-                        //    //A resembles First Column while C resembles Third column.
-                        //    //Header row is at Position 1 and hence First row starts from Index 2.
-                        //    string cellRange = string.Format("A{0}:C{0}", i + 1);
-                        //    if (i % 2 != 0)
-                        //    {
-                        //        wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.GreenYellow;
-                        //    }
-                        //    else
-                        //    {
-                        //        wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.Yellow;
-                        //    }
-
+                        //    Directory.CreateDirectory(folderPath);
                         //}
-                        //Adjust widths of Columns.
-                        wb.Worksheet(1).Columns().AdjustToContents();
+                        using (XLWorkbook wb = new XLWorkbook())
+                        {
+                            wb.Worksheets.Add(dt, Title);
 
-                        //Save the Excel file.
-                        wb.SaveAs(fichero.FileName);
-                        if (File.Exists(fichero.FileName))
-                        {
-                            Alerts.cFAT100010 alr = new Alerts.cFAT100010("Información", "Exportación Exitosa", MessageBoxIcon.Information);
-                            alr.ShowDialog();
-                        }
-                        else
-                        {
-                            Alerts.cFAT100010 alr = new Alerts.cFAT100010("Error", "Exportación Fallida", MessageBoxIcon.Error);
-                            alr.ShowDialog();
+                            ////Set the color of Header Row.
+                            ////A resembles First Column while C resembles Third column.
+                            //wb.Worksheet(1).Cells("A1:C1").Style.Fill.BackgroundColor = XLColor.DarkGreen;
+                            //for (int i = 1; i <= dt.Rows.Count; i++)
+                            //{
+                            //    //A resembles First Column while C resembles Third column.
+                            //    //Header row is at Position 1 and hence First row starts from Index 2.
+                            //    string cellRange = string.Format("A{0}:C{0}", i + 1);
+                            //    if (i % 2 != 0)
+                            //    {
+                            //        wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.GreenYellow;
+                            //    }
+                            //    else
+                            //    {
+                            //        wb.Worksheet(1).Cells(cellRange).Style.Fill.BackgroundColor = XLColor.Yellow;
+                            //    }
+
+                            //}
+                            //Adjust widths of Columns.
+                            wb.Worksheet(1).Columns().AdjustToContents();
+
+                            //Save the Excel file.
+                            wb.SaveAs(fichero.FileName);
+                            if (File.Exists(fichero.FileName))
+                            {
+                                Alerts.cFAT100010 alr = new Alerts.cFAT100010("Información", "Exportación Exitosa", MessageBoxIcon.Information);
+                                alr.ShowDialog();
+                            }
+                            else
+                            {
+                                Alerts.cFAT100010 alr = new Alerts.cFAT100010("Error", "Exportación Fallida", MessageBoxIcon.Error);
+                                alr.ShowDialog();
+                            }
                         }
                     }
+                    //SaveFileDialog fichero = new SaveFileDialog();
+                    //fichero.Filter = "Excel (*.xls)|*.xls";
+                    //if (fichero.ShowDialog() == DialogResult.OK)
+                    //{
+                    //    Microsoft.Office.Interop.Excel.Application aplicacion;
+                    //    Microsoft.Office.Interop.Excel.Workbook libros_trabajo;
+                    //    Microsoft.Office.Interop.Excel.Worksheet hoja_trabajo;
+                    //    aplicacion = new Microsoft.Office.Interop.Excel.Application();
+                    //    libros_trabajo = aplicacion.Workbooks.Add();
+                    //    hoja_trabajo =
+                    //        (Microsoft.Office.Interop.Excel.Worksheet)libros_trabajo.Worksheets.get_Item(1);
+                    //    //Recorremos el DataGridView rellenando la hoja de trabajo
+                    //    for (int i = 0; i < grd.Rows.Count - 1; i++)
+                    //    {
+                    //        for (int j = 0; j < grd.Columns.Count; j++)
+                    //        {
+                    //            hoja_trabajo.Cells[i + 1, j + 1] = grd.Rows[i].Cells[j].Value.ToString();
+                    //        }
+                    //    }
+                    //    libros_trabajo.SaveAs(fichero.FileName,
+                    //        Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
+                    //    libros_trabajo.Close(true);
+                    //    aplicacion.Quit();
+                    //}
                 }
-                //SaveFileDialog fichero = new SaveFileDialog();
-                //fichero.Filter = "Excel (*.xls)|*.xls";
-                //if (fichero.ShowDialog() == DialogResult.OK)
-                //{
-                //    Microsoft.Office.Interop.Excel.Application aplicacion;
-                //    Microsoft.Office.Interop.Excel.Workbook libros_trabajo;
-                //    Microsoft.Office.Interop.Excel.Worksheet hoja_trabajo;
-                //    aplicacion = new Microsoft.Office.Interop.Excel.Application();
-                //    libros_trabajo = aplicacion.Workbooks.Add();
-                //    hoja_trabajo =
-                //        (Microsoft.Office.Interop.Excel.Worksheet)libros_trabajo.Worksheets.get_Item(1);
-                //    //Recorremos el DataGridView rellenando la hoja de trabajo
-                //    for (int i = 0; i < grd.Rows.Count - 1; i++)
-                //    {
-                //        for (int j = 0; j < grd.Columns.Count; j++)
-                //        {
-                //            hoja_trabajo.Cells[i + 1, j + 1] = grd.Rows[i].Cells[j].Value.ToString();
-                //        }
-                //    }
-                //    libros_trabajo.SaveAs(fichero.FileName,
-                //        Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal);
-                //    libros_trabajo.Close(true);
-                //    aplicacion.Quit();
-                //}
+            } catch (Exception ex)
+            {
+                throw new Exception(string.Format("ExportExcelButton_Click: {0}", ex.Message));
             }
         }
     }
